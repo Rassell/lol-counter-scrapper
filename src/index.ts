d694ee2d2dca6d7983599af6337af136bee17c38
Gonzalo Soriano <sordesos@gmail.com>
2021-12-18 12:27:59 +0100

    fix: client connection

--- src/index.ts
+++ src/index.ts
@@ -10,78 +10,90 @@ const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 } as any);
+const championsDBCollection = client.db('lol-counter').collection('champions');
+const countersDBCollection = client.db('lol-counter').collection('counters');
 
-const characterPromises = Array.from(Array(9999).keys()).map(async i => {
-  await client.connect();
+console.log('Starting...');
 
-  try {
-    const response = await axios.get(
-      `https://cdn.communitydragon.org/latest/champion/${i}/data`,
-    );
+client
+  .connect()
+  .then(async () => {
+    console.log('Connected to database');
 
-    if (response.status > 200) return;
+    const characterPromises = Array.from(Array(9999).keys()).map(async i => {
+      try {
+        const response = await axios.get(
+          `https://cdn.communitydragon.org/latest/champion/${i}/data`,
+        );
 
-    const collection = client.db('lol-counter').collection('champions');
+        if (response.status > 200) return;
 
-    await collection.updateOne(
-      { id: i },
-      { $set: response.data },
-      { upsert: true },
-    );
+        await championsDBCollection.updateOne(
+          { id: i },
+          { $set: response.data },
+          { upsert: true },
+        );
 
-    return response.data;
-  } catch (error) {}
-});
+        console.log(`Updated ${i}`);
 
-Promise.allSettled(characterPromises).then(async dataList => {
-  const championsData: IChampionData[] = dataList
-    .filter((data: any) => data.value && data.status === 'fulfilled')
-    .map((data: any) => data.value);
+        return response.data;
+      } catch (error) {}
+    });
 
-  const browser = await puppeteer.launch({ slowMo: 1000, headless: true });
-  const page = await browser.newPage();
+    console.log(`Total champions ${characterPromises.length}`);
 
-  for (const championData of championsData) {
-    await page.goto(
-      `https://u.gg/lol/champions/${championData.name.toLowerCase()}/counter?rank=overall`,
-    );
-    await page.waitForSelector('#main-content');
+    Promise.allSettled(characterPromises).then(async dataList => {
+      const championsData: IChampionData[] = dataList
+        .filter((data: any) => data.value && data.status === 'fulfilled')
+        .map((data: any) => data.value);
 
-    const html = await page.content();
-    const $ = cheerio.load(html);
+      const browser = await puppeteer.launch({ slowMo: 1000, headless: true });
+      const page = await browser.newPage();
 
-    const selectElem = $('.counter-list-card.best-win-rate').text();
+      for (const championData of championsData) {
+        console.log(`Getting counters for ${championData.name}`);
 
-    const counters = selectElem
-      .split(' games')
-      .map(c => c.trim().replace('Matches', '').replace('WR', ''))
-      .filter(c => c !== '')
-      .map(c => {
-        const [nameWinrate, matches] = c.split('% ');
-        const name = /[a-z]+/i.exec(nameWinrate)[0];
-        const winRate = /[0-9,]+/i.exec(nameWinrate)[0];
-        return {
-          id:
-            championsData.find(c => c.name.toLowerCase() === name.toLowerCase())
-              ?.id || -1,
-          name,
-          matches: parseInt(matches.replace(',', '')),
-          winRate: parseFloat(winRate),
-        };
-      })
-      .sort((a, b) => b.winRate - a.winRate);
+        await page.goto(
+          `https://u.gg/lol/champions/${championData.name.toLowerCase()}/counter?rank=overall`,
+        );
+        await page.waitForSelector('#main-content');
 
-    const collection = client.db('lol-counter').collection('counters');
+        const html = await page.content();
+        const $ = cheerio.load(html);
 
-    await collection.updateOne(
-      { id: championData.id },
-      { $set: { counters } },
-      { upsert: true },
-    );
-  }
+        const selectElem = $('.counter-list-card.best-win-rate').text();
 
-  client.close();
+        const counters = selectElem
+          .split(' games')
+          .map(c => c.trim().replace('Matches', '').replace('WR', ''))
+          .filter(c => c !== '')
+          .map(c => {
+            const [nameWinrate, matches] = c.split('% ');
+            const name = /[a-z]+/i.exec(nameWinrate)[0];
+            const winRate = /[0-9,]+/i.exec(nameWinrate)[0];
+            return {
+              id:
+                championsData.find(
+                  c => c.name.toLowerCase() === name.toLowerCase(),
+                )?.id || -1,
+              name,
+              matches: parseInt(matches.replace(',', '')),
+              winRate: parseFloat(winRate),
+            };
+          })
+          .sort((a, b) => b.winRate - a.winRate);
 
-  await page.close();
-  await browser.close();
-});
+        await countersDBCollection.updateOne(
+          { id: championData.id },
+          { $set: { counters } },
+          { upsert: true },
+        );
+
+        console.log(`${championData.name} counters updated ${counters.length}`);
+      }
+
+      await page.close();
+      await browser.close();
+    });
+  })
+  .catch(error => console.log(`Error connecting to database: ${error}`));
