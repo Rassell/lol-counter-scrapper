fd3bdadf7c690adf09f307077ff95de96e33e8c2
Gonzalo Soriano <sordesos@gmail.com>
2021-12-18 21:04:09 +0100

    fix: correct remote config

--- terraform/function.tf
+++ terraform/function.tf
@@ -5,16 +5,16 @@ variable "CLOUDFLARE_API_KEY" {
 terraform {
   required_providers {
     cloudflare = {
-      source = "cloudflare/cloudflare"
+      source  = "cloudflare/cloudflare"
       version = "~> 3.0"
     }
   }
 
   cloud {
-    organization = "lol-counter-scrapper"
+    organization = "rassell-github"
 
     workspaces {
-      tags = ["scrapper"]
+      name = "lol-counter-scrapper"
     }
   }
 }
@@ -31,7 +31,7 @@ resource "cloudflare_worker_script" "example_script" {
 
 resource "cloudflare_worker_cron_trigger" "example_trigger" {
   script_name = cloudflare_worker_script.example_script.name
-  schedules   = [
+  schedules = [
     "*/5 * * * *",      # every 5 minutes
     "10 7 * * mon-fri", # 7:10am every weekday
   ]
