2a51e859063973a1472c65557a687bba30daa6b5
Rassell <sordesos@gmail.com>
2021-12-22 17:30:19 +0100

    pass var?

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,5 @@
+variable "DATABASE_PASS" {}
+
 terraform {
   required_providers {
     azurerm = {
@@ -77,5 +79,7 @@ resource "azurerm_function_app" "lol_counter_scrapper" {
   app_settings = {
     FUNCTIONS_WORKER_RUNTIME       = "node",
     APPINSIGHTS_INSTRUMENTATIONKEY = azurerm_application_insights.lol_counter_scrapper.instrumentation_key,
+    DATABASE_USER                  = lol-counter
+    DATABASE_PASS                  = var.DATABASE_PASS
   }
 }
